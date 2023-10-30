763ac3031dd83201a41cc55fa10d21684f784cc2
AdemBendjama <93732841+AdemBendjama@users.noreply.github.com>
2023-10-30 10:56:41 +0100

    add timer/best-record feature

--- Lab 2 Part 2/Maze_files/maze.js
+++ Lab 2 Part 2/Maze_files/maze.js
@@ -1,50 +1,60 @@
 const walls = document.querySelectorAll('.boundary');
-const stat = document.querySelector('#status')
-const end = document.querySelector('#end')
-const start = document.querySelector('#start')
-let gameStarted = false
+const stat = document.querySelector('#status');
+const end = document.querySelector('#end');
+const start = document.querySelector('#start');
+const timer = document.querySelector('#timer');
+const bestRecord = document.querySelector('#bestRecord');
+
+let gameStarted = false;
+let startTime = 0;
+let bestTime = null;
 
 walls.forEach(wall => {
     wall.addEventListener("mouseover", (e) => {
         if (gameStarted) {
             walls.forEach(wall => {
                 wall.style.backgroundColor = "red";
-                stat.innerHTML = "You Lose !"
-                gameStarted = false
-            })
+                stat.innerHTML = "You Lose !";
+                gameStarted = false;
+            });
         }
-    })
+    });
 });
 
 end.addEventListener('mouseover', (e) => {
-
-    wallsCurrentColor = walls.item(1).style.backgroundColor
+    wallsCurrentColor = walls.item(1).style.backgroundColor;
     if (wallsCurrentColor !== "red" && gameStarted) {
-        stat.innerHTML = "You Win !"
-        gameStarted = false
+        stat.innerHTML = "You Win !";
+        gameStarted = false;
+        const endTime = Date.now();
+        const elapsedTime = (endTime - startTime) / 1000;
+        timer.innerHTML = `Time taken: ${elapsedTime} seconds`;
+
+        if (bestTime === null || elapsedTime < bestTime) {
+            bestTime = elapsedTime;
+            bestRecord.innerHTML = `Best Record: ${bestTime} seconds`;
+        }
     }
-})
+});
 
 start.addEventListener('mousedown', (e) => {
     walls.forEach(wall => {
-        wall.style.backgroundColor = "#eeeeee"
-        gameStarted = true
-        stat.innerHTML = "New Game Started !"
-    })
+        wall.style.backgroundColor = "#eeeeee";
+        gameStarted = true;
+        stat.innerHTML = "New Game Started !";
+        startTime = Date.now();
+    });
+});
 
-})
-
-
-const outside = document.querySelectorAll('div#maze')
-console.log(outside);
+const outside = document.querySelectorAll('div#maze');
 outside.forEach((element) => {
     element.addEventListener("mouseleave", (e) => {
         if (gameStarted) {
             walls.forEach(wall => {
                 wall.style.backgroundColor = "red";
-                stat.innerHTML = "Don't Cheat !"
-                gameStarted = false
-            })
+                stat.innerHTML = "Don't Cheat !";
+                gameStarted = false;
+            });
         }
-    })
-})
+    });
+});
